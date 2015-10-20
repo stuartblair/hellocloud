54319e6fa9a44070cf4515e8741e54a7b69eecdf
Stuart Blair <stuart_blair@gap.com>
2015-10-20 11:40:22 -0700

    Initial commit

---
+++ src/main/java/com/gap/HelloCloudApplication.java
@@ -0,0 +1,12 @@
+package com.gap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCloudApplication.class, args);
+    }
+}
